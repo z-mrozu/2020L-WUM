1707627f7fac8606aa06286f53261b1e27cbf81f
Z-Xbeova <mwpk310@gmail.com>
2020-03-08 18:46:47 +0100

    eksploracje w raporcie; pozostało użycie create_report

--- Prace_domowe/Praca_domowa1/Grupa1/Korbin_Marceli/kredyt.R
+++ Prace_domowe/Praca_domowa1/Grupa1/Korbin_Marceli/kredyt.R
@@ -1,6 +1,20 @@
+kredyty <- read.csv("german_credit_data_weka_dataset.csv")
 library(rpivotTable)
+library(ggplot2)
+library(dplyr)
+
 rpivotTable(kredyty, cols = "present_employment", rows = "purpose", aggregatorName="Count", vals = "Value",
             rendererName = "Col Heatmap", height=400, subtotals=FALSE)
 
 # bezrobotni nie chcą radia i telewizji
-# to be continued later
+
+age_dur <- kredyty %>% group_by(age, duration) %>% count()
+colnames(age_dur) <- c("age", "duration", "liczbaOsób")
+
+ggplot(age_dur, aes(x=duration, y=age)) + geom_point(aes(colour=liczbaOsób, size=liczbaOsób)) + guides(size=FALSE)
+
+# zazwyczaj kredyt jest brany na rok~2 lata przez ludzi do 40 lat
+
+# credit_history & credit_amount
+
+ggplot(kredyty, aes(x=credit_history, y=credit_amount)) + geom_boxplot() + coord_flip()
