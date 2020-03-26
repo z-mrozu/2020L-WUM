67a3d3eb32e9af3adf5645071f3fc7344264e85e
MaroonBlue <martyna.majchrzak19@gmail.com>
2020-03-26 16:19:12 +0100

    Praca domowa 2

--- Projekty/Projekt1/Grupa3/MajchrzakRólkiewiczMakarewicz/exploracja.R
+++ Projekty/Projekt1/Grupa3/MajchrzakRólkiewiczMakarewicz/exploracja.R
@@ -1,26 +1,3 @@
-DataExplorer::create_report(data, 
-                            output_file = "MajchrzakMartyna_pd1_report.html", 
-                            report_title = "Heart Disease Data Profiling Report"
-                            )
-
-
-
-data_cancer<-read.csv("../data/data/cervical-cancer.csv")
-DataExplorer::create_report(data_cancer, output_file = "cancer_report.html", report_title = "Cervical Cancer Data Profiling Report")
-
-DataExplorer::plot_boxplot(data, by = "Dx.Cancer")
-DataExplorer::plot_boxplot(data, by = "sex")
-DataExplorer::plot_boxplot(data, by = "age")
-
-data_marketing<-read.csv("../data/bank_marketing_dataset.csv")
-DataExplorer::create_report(data_marketing, output_file = "marketing_report.html", report_title = "Bank Marketing Profiling Report")
-
-data_mammography<-read.csv("../data/mammography.csv")
-DataExplorer::create_report(data_mammography, 
-                            output_file = "marketing_report.html",
-                            output_dir = "./Projekty/Projekt1/Grupa3/MajchrzakRólkiewiczMakarewicz",
-                            report_title = "Mammography Profiling Report")
-
 data_breast<-read.csv("../data/breast-w_csv.csv")
 summary(data_breast)
 data_breast$Class<-as.factor(data_breast$Class)
